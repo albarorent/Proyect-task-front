1188300977b4cb0f7770a089181d4ba677955f68
Alvaro <alvarorent2001@gmail.com>
2024-04-10 00:32:54 -0500

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
-  // baseURL: "https://api-tasks-u571.onrender.com/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "https://api-tasks-u571.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
